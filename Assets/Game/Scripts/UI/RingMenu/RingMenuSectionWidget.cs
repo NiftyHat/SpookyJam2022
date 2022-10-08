1e58eb7d6f2872b54f4709b3210853d74288ecfa
Duncan "NiftyHat" Saunders <titch007uk@hotmail.com>
2022-10-07 19:04:50 -0700

    Ring menu set to re-use internal prefab correctly.

--- Assets/Game/Scripts/UI/RingMenu/RingMenuSectionWidget.cs
+++ Assets/Game/Scripts/UI/RingMenu/RingMenuSectionWidget.cs
@@ -8,17 +8,32 @@ namespace UI
         [SerializeField] private Image _background;
         [SerializeField] private Image _icon;
 
+        private int _index = 0;
+
         public Image Icon => _icon;
         public Image Background => _background;
 
-        public void Set(RingMenuItemData menuItem)
+        public int Index => _index;
+
+        public void Set(RingMenuItemData menuItem, int index)
         {
             _icon.sprite = menuItem.Icon;
+            _index = index;
         }
 
         public float GetIconDistance()
         {
             return Vector3.Distance(this.transform.position, _icon.transform.position);
+        }
+
+        public void PointerExit()
+        {
+            _background.color = Color.black;
+        }
+
+        public void PointerOver()
+        {
+            _background.color = Color.gray;
         }
     }
 }
--- Assets/Game/Scripts/UI/RingMenu/RingMenuWidget.cs
+++ Assets/Game/Scripts/UI/RingMenu/RingMenuWidget.cs
@@ -1,8 +1,9 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityUtils;
 
-namespace UI.RingMenu
+namespace UI
 {
     public class RingMenuWidget : MonoBehaviour
     {
@@ -12,12 +13,19 @@ namespace UI.RingMenu
         [SerializeField] RingMenuSectionWidget _sectionPrefab;
         [SerializeField] protected float _sectionSpacing = 1f;
 
+        private MonoPool<RingMenuSectionWidget> _sectionPool;
+        private List<RingMenuSectionWidget> _sectionWidgets = new List<RingMenuSectionWidget>();
+        private int _lastSelectedSection = -1;
+        private RingMenuSectionWidget _lastSelectedWidget;
 
+        private float _stepLength = 0;
         public event Action OnSelected;
 
         // Start is called before the first frame update
         void Start()
         {
+            _sectionPool = new MonoPool<RingMenuSectionWidget>(_sectionPrefab);
+            
             _model = _ringMenu.Create();
             int itemCount = _model.Count;
             if (itemCount == 0)
@@ -25,23 +33,25 @@ namespace UI.RingMenu
                 return;
             }
 
-            float stepLength = 360f / itemCount;
+            _stepLength = 360f / itemCount;
             float iconDist = _sectionPrefab.GetIconDistance();
             float length = _ringMenu.References.Count;
 
             for (int i = 0; i < length; i++)
             {
                 var sectionData = _ringMenu.References[i];
-                var sectionInstance = Instantiate(_sectionPrefab, Vector3.one, Quaternion.identity, transform);
+                _sectionPool.TryGet(out var sectionInstance);
+                //var sectionInstance = Instantiate(_sectionPrefab, Vector3.one, Quaternion.identity, transform);
                 sectionInstance.Background.fillAmount = (1f / length) - _sectionSpacing / 360f;
                 sectionInstance.Background.transform.localPosition = Vector3.zero;
-                sectionInstance.Background.transform.localRotation = Quaternion.Euler(0, 0, -stepLength / 2f + _sectionSpacing + i * stepLength);
+                sectionInstance.Background.transform.localRotation = Quaternion.Euler(0, 0, -_stepLength / 2f + _sectionSpacing + i * _stepLength);
                 sectionInstance.Background.color = Color.black;
 
                 sectionInstance.Icon.transform.localPosition = Vector3.zero +
-                                                               Quaternion.AngleAxis(i * stepLength, Vector3.forward) *
+                                                               Quaternion.AngleAxis(i * _stepLength, Vector3.forward) *
                                                                Vector3.up * iconDist;
-                sectionInstance.Set(sectionData);
+                sectionInstance.Set(sectionData, i);
+                _sectionWidgets.Add(sectionInstance);
             }
             
         }
@@ -49,7 +59,31 @@ namespace UI.RingMenu
         // Update is called once per frame
         void Update()
         {
-        
+            var mouseAngle =
+                NormalizeAngle(
+                    Vector3.SignedAngle(Vector3.up, Input.mousePosition - transform.position, Vector3.forward) +
+                    _stepLength / 2f);
+            int mouseOverSectionIndex = (int)(mouseAngle / _stepLength);
+            if (mouseOverSectionIndex != _lastSelectedSection)
+            {
+                _lastSelectedSection = mouseOverSectionIndex;
+                if (mouseOverSectionIndex >= 0 && mouseOverSectionIndex <= _sectionWidgets.Count)
+                {
+                    var selectedItem = _sectionWidgets[mouseOverSectionIndex];
+                    if (_lastSelectedWidget != selectedItem)
+                    {
+                        if (_lastSelectedWidget != null)
+                        {
+                            _lastSelectedWidget.PointerExit();
+                            _lastSelectedWidget = null; 
+                        }
+                        selectedItem.PointerOver();
+                        _lastSelectedWidget = selectedItem;
+                    }
+                }
+            }
         }
+
+        private float NormalizeAngle(float a) => (a + 360f) % 360;
     }
 }
