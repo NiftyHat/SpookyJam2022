201241cc4f4de7e2c7932fcef4d562380d5c1f1f
kdyung <kyung8888@gmail.com>
2022-10-11 22:35:34 -0700

    Masked Guest Menu widget data object

--- Assets/Game/Scripts/UI/MaskedGuestMenu/MaskedGuestMenuWidget.cs
+++ Assets/Game/Scripts/UI/MaskedGuestMenu/MaskedGuestMenuWidget.cs
@@ -5,17 +5,6 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public enum LocationsSeen
-{
-    None        = 0,      
-    Balcony     = 1 << 0, 
-    UpperLanding = 1 << 1, 
-    Buffet      = 1 << 2, 
-    Stairs      = 1 << 4,
-    Garden      = 1 << 5,
-    LowerLanding = 1 << 6,
-    DanceFloor = 1 << 7
-}
 
 public class MaskedGuestMenuWidget : MonoBehaviour
 {
