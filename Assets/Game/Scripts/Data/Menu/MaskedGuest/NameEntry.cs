6cb1fa9afafc6cc93f04ff82e3196e6a8ea9490b
kdyung <kyung8888@gmail.com>
2022-10-12 23:52:31 -0700

    Guest List name entry thing

--- Assets/Game/Scripts/Data/Menu/MaskedGuest/NameEntry.cs
+++ Assets/Game/Scripts/Data/Menu/MaskedGuest/NameEntry.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class NameEntry : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TextMeshProUGUI nameText;
+    public Button button;
+
+    public void Start()
     {
-        
+        //button.onClick.AddListener();
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    public void SetName(string name)
     {
-        
+        nameText.SetText(name);
     }
 }
