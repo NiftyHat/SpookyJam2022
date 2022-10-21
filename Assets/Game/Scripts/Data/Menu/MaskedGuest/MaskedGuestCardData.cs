98cb3d38eaf9e5481177254afa806204149b05be
kdyung <kyung8888@gmail.com>
2022-10-20 23:39:41 -0700

    Updated card widget to display data. Location and traits shown as icons.

--- Assets/Game/Scripts/Data/Menu/MaskedGuest/MaskedGuestCardData.cs
+++ Assets/Game/Scripts/Data/Menu/MaskedGuest/MaskedGuestCardData.cs
@@ -1,12 +1,28 @@
 using Data.Area;
+using Data.Mask;
+using Data.Style;
 using Data.Trait;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 //Data object for Player's notes on masked guests
+[Serializable]
 public class MaskedGuestCardData
 {
+    public MaskData mask;
+    public ColorStyleData maskColor;
+    public string DisplayName
+    {
+        get 
+        {
+            if (string.IsNullOrEmpty(name))
+                return String.Format("{0} {1}", maskColor.FriendlyName, mask.FriendlyName);
+            else
+                return name;
+        }
+    }
     public string name;
     public List<AreaData> locationData;
     public List<TraitData> traitData;
