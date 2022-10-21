4a5167c6aa041fa1307bcf3e58ba641e86592129
kdyung <kyung8888@gmail.com>
2022-10-20 19:53:04 -0700

    Update masked guest card widget

---
+++ Assets/Game/Scripts/UI/MaskedGuestMenu/IconWidget.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IconWidget : MonoBehaviour
+{
+    [SerializeField]
+    private Image image;
+
+    public void SetSprite(Sprite sprite)
+    {
+        image.sprite = sprite;
+    }
+}
--- Assets/Game/Scripts/UI/MaskedGuestMenu/MaskedGuestCardWidget.cs
+++ Assets/Game/Scripts/UI/MaskedGuestMenu/MaskedGuestCardWidget.cs
@@ -8,12 +8,40 @@ using UnityEngine.UI;
 
 public class MaskedGuestCardWidget : MonoBehaviour
 {
-    public Image guestPortrait;
-    public TextMeshProUGUI nameDisplayText;
-    public TextMeshProUGUI locationDisplayText;
-    public TextMeshProUGUI tagDisplayText;
     [SerializeField]
-    private Image iconPrefab;
+    private IconWidget iconPrefab;
+    private MaskedGuestCardData data;
 
+    [Header("Display Data")]
+    public Image guestPortrait;
+
+    public TextMeshProUGUI nameDisplayText;
+
+    public Transform locationContainer, traitContainer;
+
+    private List<IconWidget> locationIcons;
+    private List<IconWidget> traitIcons;
+
+
+    public void Initialize(MaskedGuestCardData data)
+    {
+        this.data = data;
+        nameDisplayText.SetText(data.name);
+        foreach (AreaData loc in data.locationData)
+        {
+            IconWidget icon = GameObject.Instantiate<IconWidget>(iconPrefab, locationContainer);
+            icon.SetSprite(loc.Icon);
+        }
+        foreach (TraitData trt in data.traitData)
+        {
+            IconWidget icon = GameObject.Instantiate<IconWidget>(iconPrefab, traitContainer);
+            icon.SetSprite(trt.Icon);
+        }
+    }
+
+    public void ShowDetailedData(bool show)
+    {
+
+    }
 
 }
