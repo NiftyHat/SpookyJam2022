e24773f135e423f16c3d792537fd98c748a47f70
Duncan "NiftyHat" Saunders <titch007uk@hotmail.com>
2022-10-26 22:45:28 -0700

    Added Sprite-WithDepth.mat, Updated monsters to have 3 pref traits, Added new monster traits. Added a camera following the player. Set all the mask sprites.

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -2,32 +2,37 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    protected Vector3 _distanceToTarget;
-
-    protected Transform _followTarget;
+    protected Vector3 _relativeVectorFromTarget;
+    
     protected Vector3 _lookAtPoint;
 
-    [SerializeField] private Transform _transform;
+    [SerializeField] private Transform _followTarget;
     [SerializeField] private float _distanceToMove = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     void SetTarget(Transform followTarget)
     {
-        _distanceToTarget = transform.position - _followTarget.position;
+        _followTarget = followTarget;
+        _relativeVectorFromTarget = transform.position - _followTarget.position;
+        _lookAtPoint = transform.position + _followTarget.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_lookAtPoint == Vector3.zero)
+        {
+            SetTarget(_followTarget);
+        }
         Vector3 distance = _lookAtPoint - _followTarget.position;
         if (distance.magnitude > _distanceToMove)
         {
-            Vector3 newCameraTargetPos = _followTarget.position;
-            transform.position = Vector3.Lerp(transform.position, newCameraTargetPos, 0.1f);
+            Vector3 newCameraTargetPos = _followTarget.position + _relativeVectorFromTarget;
+            transform.position = Vector3.Lerp(transform.position, newCameraTargetPos, 0.005f);
+            _lookAtPoint = transform.position - _relativeVectorFromTarget;
         }
     }
 }
