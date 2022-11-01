e6e9074037d903337bebfcf66a2c2882c27d2470
kdyung <kyung8888@gmail.com>
2022-10-31 22:24:24 -0700

    added a context

--- Assets/Game/Scripts/Context/MaskGuessContext.cs
+++ Assets/Game/Scripts/Context/MaskGuessContext.cs
@@ -1,4 +1,5 @@
 using Data;
+using Entity;
 using NiftyFramework.Core.Context;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,11 +8,24 @@ namespace Context
 {
     public class MaskGuessContext : IContext
     {
-        private List<MaskGuessCardData> _maskGuessData;
+        private Dictionary<MaskEntity, MaskGuessCardData> _maskGuessData;
+
+        public void CreateNewEntry(MaskEntity mask)
+        {
+            if (!_maskGuessData.ContainsKey(mask))
+            {
+                MaskGuessCardData newData = new MaskGuessCardData(mask);
+                _maskGuessData.Add(mask, newData);
+            }
+        }
+
+        public void EditEntry(MaskGuessCardData data)
+        {
+            _maskGuessData[data.mask] = data;
+        }
 
         public void Dispose()
         {
-            throw new System.NotImplementedException();
         }
     }
 }
